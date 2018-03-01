8d112bbd078f5ae6b365631d66953cbf6bc53dac
syz <1091969093@qq.com>
2018-03-01 17:55:47 +0800

    update news page

--- news/js/news.js
+++ news/js/news.js
@@ -2,23 +2,56 @@ require.config({
     paths: {
         "jquery": "https://cdn.bootcss.com/jquery/2.1.1/jquery",
         "common": "../../js/common",
-        "imagesLoaded": "https://unpkg.com/imagesloaded@4/imagesloaded.pkgd"
+        // "imagesLoaded": "https://unpkg.com/imagesloaded@4/imagesloaded.pkgd"
     },
-    shim: {
-        "imagesLoaded": {
-            deps: ["jquery"]
-        }
-    }
+    // shim: {
+    //     "imagesLoaded": {
+    //         deps: ["jquery"]
+    //     }
+    // }
 });
-require(["jquery", "common", "imagesLoaded"], function ($, common) {
-    var video_outer_box = $("#video-outer-box"),
-        morevideo_icon = $(".morevideo-icon");
-    $("#video-wrapper").imagesLoaded().progress(function (instance, image) {
-        $(image.img).css('opacity', 1);
-    });
+require(["jquery", "common"], function ($, common) {
+    var page = 1;
 
-    morevideo_icon.click(function () {
-        for (var i = 0; i < 3; i++) {
-        }
+    function morenews() {
+        $.ajax({
+            type: 'post',
+            async: false,
+            data: {'page': page},
+            url: "http://www.meflourish.com/mfgw.php/Index/getnews",
+            success: function (data) {
+                if (data == null) {
+                    $("#morevideo-box").find("span").html("没有更多了");
+                } else {
+                    page++;
+                    for (var i = 0; i < data.length; i++) {
+                        var tpl = '<div class="news-item clearfix">'
+                            + '<div class="news-time-box">'
+                            + '<div class="news-time">'
+                            + '<p class="news-month">' + data[i].month + '月</p>'
+                            + '<p class="news-day">' + data[i].day + '</p>'
+                            + '</div>'
+                            + '</div>'
+                            + '<div class="news-msg-box">'
+                            + '<p class="news-tag">' + data[i].tag + '</p>'
+                            + '<p class="news-title ' + data[i].type + '-box getpop">' + data[i].title + '<span class="video-src">' + data[i].url + '</span></p>'
+                            + '<p class="news-intro">' + data[i].intro + '...</p>'
+                            + '</div>'
+                            + '<div class="news-img-box">'
+                            + '<img src="../article/img/chunwan.png" alt=""></div></div>'
+                        $("#news-wrapper").append(tpl);
+                    }
+                }
+                common.reloadvideo();
+            },
+            error: function () {
+                alert("发生了点错误，请重新尝试");
+            }
+        })
+    }
+
+    morenews();
+    $(".morevideo-icon").click(function () {
+        morenews();
     })
 });
